82a5cf86e06a33eee7f93b6b86b6ba054a12d5f1
Marcio Matsushita <marcioymatsushita@outlook.com>
2022-09-15 11:38:02 -0300

    Nova feature adicionada para busca utilizando de 3 parametros, nome, tipo e capturado, sendo apenas 1 deles obrigatório e os demais opcionais

--- api/controllers/pokemonController.js
+++ api/controllers/pokemonController.js
@@ -12,15 +12,78 @@ class PokeController {
     }
     static async buscaPoke(req, res) {
         const nome = req.query.nome
-        
         try {
             const Pokes = await database.Pokemon.findAll( {
                 where: {
                     nome: nome
-           
                     }
             })
             return res.status(200).json(Pokes)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    static async buscaCompleta(req, res) {
+        const nome = req.query.nome
+        const tipo = req.query.tipo
+        const capturado = req.query.capturado
+        
+        try {
+            // busca com todos os itens
+            if (nome != undefined && tipo != undefined && capturado != undefined) {
+                const PokeCompleto = await database.Pokemon.findAll( {
+                    where: {
+                        nome: nome,
+                        tipo: tipo,
+                        capturado: capturado}
+                }) 
+                return res.status(200).json(PokeCompleto)    
+            } if (tipo === undefined && capturado === undefined) {                
+                const PokeCompleto = await database.Pokemon.findAll( {
+                    where: { 
+                        nome: nome
+                    }
+                }) 
+                return res.status(200).json(PokeCompleto)
+            } if (nome === undefined && tipo === undefined) {                
+                const PokeCompleto = await database.Pokemon.findAll( {
+                    where: { 
+                    capturado: capturado
+            }
+                })
+                return res.status(200).json(PokeCompleto)
+            } if (nome === undefined && capturado === undefined) {                
+                const PokeCompleto = await database.Pokemon.findAll( {
+                    where: { 
+                        tipo: tipo
+                }
+                })
+                return res.status(200).json(PokeCompleto)
+            } if (tipo === undefined) {                
+                const PokeCompleto = await database.Pokemon.findAll( {
+                    where: { 
+                        nome: nome,
+                        capturado: capturado
+                    }
+                })
+                return res.status(200).json(PokeCompleto)  
+            } if (capturado === undefined) {                
+                const PokeCompleto = await database.Pokemon.findAll( {
+                    where: { 
+                        nome: nome,
+                        tipo: tipo
+                    }
+                })
+                return res.status(200).json(PokeCompleto)
+            } if (nome === undefined) {                
+            const PokeCompleto = await database.Pokemon.findAll( {
+                where: { 
+                capturado: capturado,
+                tipo: tipo
+            }
+            })
+                return res.status(200).json(PokeCompleto)
+}  
         } catch (error) {
             return res.status(500).json(error.message)
         }
